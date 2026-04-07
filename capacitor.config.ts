6ce44bf5afdf08a78dc2269c377b7174413ef2bd
Mohamed Haiballa <mohammedcheikhtijanihaiballa@gmail.com>
2026-04-07 11:01:05 +0000

    i18n boutton absent

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'mr.eetfp.attendance',
-  appName: 'EETFP-MPG Présence',
-  webDir: 'dist'
-};
-
-export default config;
